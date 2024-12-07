f203ac74f9edb809af755970f1c81bbf61886e7f
shota-nagato <shota.nagato@gmail.com>
2024-12-07 18:00:40 +0900

    ログイン後のレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  authenticated :user do
+    root to: "dashboard#show", as: :user_root
+  end
+
   devise_scope :user do
     root "devise/registrations#new"
   end
