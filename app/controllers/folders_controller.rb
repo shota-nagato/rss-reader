1da5e6d55cbcc42cf4a0cb8561b2efde8f11373c
shota-nagato <shota.nagato@gmail.com>
2024-12-10 01:34:48 +0900

    フォルダを編集できる

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,6 +1,6 @@
 class FoldersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_folder, only: %i[show destroy]
+  before_action :set_folder, only: %i[show edit update destroy]
 
   def index
   end
@@ -12,17 +12,33 @@ class FoldersController < ApplicationController
     @folder = Folder.new
   end
 
+  def edit
+  end
+
   def create
     @folder = current_user.folders.new(folder_params)
 
     respond_to do |format|
       if @folder.save
         format.turbo_stream {
-          flash[:notice] = "フォルダーを作成しました"
+          flash[:notice] = "フォルダを作成しました"
           render turbo_stream: turbo_stream.action(:redirect, folder_path(@folder))
         }
       else
         format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @folder.update(folder_params)
+        format.turbo_stream {
+          flash[:notice] = "フォルダを編集しました"
+          render turbo_stream: turbo_stream.action(:redirect, folder_path(@folder))
+        }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
