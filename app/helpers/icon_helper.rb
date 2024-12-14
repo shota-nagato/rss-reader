87c3023966c2ad36c7c51c7cb5703baadbcba826
shota-nagato <shota.nagato@gmail.com>
2024-12-14 16:10:34 +0900

    icon表示用のヘルパー作成

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,10 @@
+module IconHelper
+  def icon(name, css: nil)
+    file_path = File.read(Rails.root.join("app", "assets", "svgs", "#{name}.svg"))
+    svg_file = Nokogiri::HTML::DocumentFragment.parse(file_path).at_css("svg")
+
+    svg_file["class"] = css unless css.nil?
+
+    svg_file.to_html.html_safe
+  end
+end
