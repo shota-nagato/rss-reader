3624ba2cba950f6c0e6b623d93d65741d029b12b
shota-nagato <shota.nagato@gmail.com>
2024-12-17 20:24:24 +0900

    並び順を指定

--- app/controllers/folders/feeds_controller.rb
+++ app/controllers/folders/feeds_controller.rb
@@ -4,7 +4,7 @@ class Folders::FeedsController < ApplicationController
 
   def show
     @feed = @folder.feeds.find(params[:id])
-    @items = Item.user_feed_items(@feed, current_user)
+    @items = Item.user_feed_items(@feed, current_user).order(published_at: :desc)
 
     # 購読直後は記事が0なので初めてfeed詳細遷移するタイミングで保存
     @feed.update_user_items(current_user) if @items.count.zero?
