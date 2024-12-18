026dd0858be04801f25918e6bc1c72824ecca792
shota-nagato <shota.nagato@gmail.com>
2024-12-18 21:50:56 +0900

    UI修正

--- app/javascript/controllers/change_view_controller.js
+++ app/javascript/controllers/change_view_controller.js
@@ -13,12 +13,12 @@ export default class extends Controller {
       items.classList.remove("grid")
       items.classList.remove("md:grid-cols-2")
       items.classList.remove("lg:grid-cols-4")
-      items.classList.remove("gap-4")
+      items.classList.remove("gap-2")
     } else {
       items.classList.add("grid")
       items.classList.add("md:grid-cols-2")
       items.classList.add("lg:grid-cols-4")
-      items.classList.add("gap-4")
+      items.classList.add("gap-2")
     }
   }
 }
