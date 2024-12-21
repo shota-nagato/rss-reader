7e74dff34085dc62fb4c91fa5bf9ce5ef7b73252
shota-nagato <shota.nagato@gmail.com>
2024-12-21 17:32:01 +0900

    表示切り替えの処理を修正

--- app/controllers/feeds/updates_controller.rb
+++ app/controllers/feeds/updates_controller.rb
@@ -9,9 +9,12 @@ class Feeds::UpdatesController < ApplicationController
 
     feed.update_user_items(current_user)
 
+    items = feed.items.order(published_at: :desc)
+    items = items.eager_load(:user_items).where(user_items: {user: current_user, liked: true}) if params[:liked].present?
+
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.update(:items, partial: "items/item", collection: feed.items, as: :item, locals: {type: "card"})
+        render turbo_stream: turbo_stream.update(:items, partial: "items/item", collection: items, as: :item, locals: {type: params[:type]})
       end
     end
   end
